54955be64441624cb95d714bddcc9dd00619ce93
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-11-20 22:08:51 -0500

    Prefer || over ternary when possible

--- lib/thread_variable.rb
+++ lib/thread_variable.rb
@@ -4,12 +4,12 @@ module ThreadVariable
   def thread_variable *names
     names.each do |name|
       define_singleton_method :"#{name}" do
-        namespace = (self.name ? self.name : self.object_id.to_s).to_sym
+        namespace = (self.name || self.object_id.to_s).to_sym
         (Thread.current[namespace] ||= {})[name]
       end
 
       define_singleton_method :"#{name}=" do |val|
-        namespace = (self.name ? self.name : self.object_id.to_s).to_sym
+        namespace = (self.name || self.object_id.to_s).to_sym
         (Thread.current[namespace] ||= {})[name] = val
       end
     end
