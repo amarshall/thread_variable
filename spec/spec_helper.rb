0e29b02e22f47aa7e002f092f599417d731f6749
Andrew Marshall <andrew@johnandrewmarshall.com>
2013-05-27 00:43:29 -0400

    Use RSpec's "new" expect syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,8 @@ require 'thread_variable'
 
 RSpec.configure do |config|
   config.color_enabled = true
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
--- spec/thread_variable_spec.rb
+++ spec/thread_variable_spec.rb
@@ -10,20 +10,20 @@ describe ThreadVariable do
 
   before do
     # Normally I wouldn't do this, but threads are special
-    klass.my_var.should be_nil
+    expect(klass.my_var).to be_nil
   end
 
   it "can set and retrieve a thread variable" do
     klass_var = klass.my_var = double
-    klass.my_var.should == klass_var
+    expect(klass.my_var).to eq klass_var
   end
 
   it "is local to a thread" do
     klass_var = klass.my_var = double
     Thread.new do
-      klass.my_var.should be_nil
+      expect(klass.my_var).to be_nil
     end.join
-    klass.my_var.should == klass_var
+    expect(klass.my_var).to eq klass_var
   end
 
   it "namespaces thread variables for anonymous classes" do
@@ -35,8 +35,8 @@ describe ThreadVariable do
     klass_var = klass.my_var = double
     other_var = other_klass.my_var = double
 
-    klass.my_var.should == klass_var
-    other_klass.my_var.should == other_var
+    expect(klass.my_var).to eq klass_var
+    expect(other_klass.my_var).to eq other_var
   end
 
   it "namespaces thread variables for named classes" do
@@ -49,7 +49,7 @@ describe ThreadVariable do
     c_var = C.my_var = double
     d_var = D.my_var = double
 
-    C.my_var.should == c_var
-    D.my_var.should == d_var
+    expect(C.my_var).to eq c_var
+    expect(D.my_var).to eq d_var
   end
 end
