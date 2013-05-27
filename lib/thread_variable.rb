b5632d86f8f6f882ee873b03255931ea34f512e1
Andrew Marshall <andrew@johnandrewmarshall.com>
2013-05-27 00:37:06 -0400

    Define namespace outside of method to avoid recalculating
    Since the method is defined as a closure, we can pre-calculate it and it
    will always use that namespace. This should give a slight speed-up for
    the getters and setters.

--- lib/thread_variable.rb
+++ lib/thread_variable.rb
@@ -3,13 +3,13 @@ require 'thread_variable/version'
 module ThreadVariable
   def thread_variable *names
     names.each do |name|
+      namespace = (self.name || self.object_id.to_s).to_sym
+
       define_singleton_method :"#{name}" do
-        namespace = (self.name || self.object_id.to_s).to_sym
         (Thread.current[namespace] ||= {})[name]
       end
 
       define_singleton_method :"#{name}=" do |val|
-        namespace = (self.name || self.object_id.to_s).to_sym
         (Thread.current[namespace] ||= {})[name] = val
       end
     end
